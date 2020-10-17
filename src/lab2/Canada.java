bcf4463f81fb8f590c668d2277ac2c9a5c08b1ff
Sh11ntar0 <sh11ntar0h@gmail.com>
2020-10-17 15:22:10 -0700

    first commit

---
+++ src/lab2/Canada.java
@@ -0,0 +1,11 @@
+package lab2;
+
+public class Canada {
+
+    public void displayAllProvinces() {
+    }
+
+    public boolean getNumOfProvincesBetween(int i, int i1) {
+    return false;
+    }
+}
